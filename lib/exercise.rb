5b46f7477f04904c4c3d085f9d2126cfedb79194
Bobby Blackstock <bobbyblackstock@gmail.com>
2014-07-28 11:59:43 -0600

    upsate items

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -4,4 +4,6 @@ require "./lib/item"
 require "./lib/orderitem"
 require "./lib/order"
 
-p Customer.where(:state => ["Ohio", "Virginia"])
+item = Item.find_by(description: "snow board")
+item.update(name: "Board01")
+p item
