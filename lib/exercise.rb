3df283774c136385947c79a940d8c8cc99476be8
Bobby Blackstock <bobbyblackstock@gmail.com>
2014-07-28 16:15:19 -0600

    orders and customers ids

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -6,6 +6,6 @@ require "./lib/order"
 
 
 Order.select("*").joins(orderitems: :item).where("orders.customer_id" => 2).each do |thing|
-   thing[:name]
-   thing[:customer_id]
+  p thing[:name]
+  p thing[:customer_id]
 end
