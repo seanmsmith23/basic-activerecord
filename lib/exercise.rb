0f67d105ed3e1a8966b0b3a690c1be811d2bb8bb
Bobby Blackstock <bobbyblackstock@gmail.com>
2014-07-28 13:05:37 -0600

    destroy the kayak

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -4,5 +4,5 @@ require "./lib/item"
 require "./lib/orderitem"
 require "./lib/order"
 
-Item.create(name: "kayak01", description: "one person river kayak")
+Item.find_by(name: "kayak01").destroy
 p Item.all
