9068a294512b5e14ce9ba06b4c3384abed87590f
Bobby Blackstock <bobbyblackstock@gmail.com>
2014-07-28 16:18:56 -0600

    item boots02

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -5,7 +5,7 @@ require "./lib/orderitem"
 require "./lib/order"
 
 
-Order.select("*").joins(orderitems: :item).where("orders.customer_id" => 2).each do |thing|
+Order.select("*").joins(orderitems: :item).where("items.name" => "boot02").each do |thing|
   p thing[:name]
   p thing[:customer_id]
 end
